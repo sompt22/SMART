1b42b7f3ce6f422a8a052724ff397c70a8fe9058
sompt22 <femres87@gmail.com>
2024-05-10 17:31:24 +0300

    --pre_hm flag removed, max thresh raised to 150

--- experiments/test.sh
+++ experiments/test.sh
@@ -5,11 +5,11 @@ model_path=$(realpath $1)
 
 
 cd src
-python test.py tracking,embedding --exp_id sompt22-train-10secs-occlusion-noprehm \
-                                  --dataset sompt22-train-10secs-occlusion \
+python test.py tracking,embedding --exp_id sompt22-train-10secs-clear-max150-noprehm-kd10 \
+                                  --dataset sompt22-train-10secs-clear \
                                   --trainval \
                                   --ltrb_amodal \
-                                  --max_age 15 \
+                                  --max_age 150 \
                                   --track_thresh 0.4 \
                                   --pre_thresh 0.5 \
                                   --load_model $model_path \
